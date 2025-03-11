257ff5662ee463c254d889df342493f89297aac2
Isaac-Gregory <isaac.gregory.csce@gmail.com>
2025-03-11 08:15:34 -0500

    Removed firebase info from GitHub for privacy

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js';
 import { getFirestore, collection, getDocs, addDoc, query, where } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js';
+import firebaseConfig from "./firebase-config.js";
 // import { initializeApp } from 'firebase/app';
 // import { getFirestore, collection, getDocs, addDoc, query, where } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyA02aFR3zCRNJP3s--BxLXm3WobK0kWQb8",
-    authDomain: "beat-roulette.firebaseapp.com",
-    projectId: "beat-roulette",
-    storageBucket: "beat-roulette.firebasestorage.app",
-    messagingSenderId: "973708648462",
-    appId: "1:973708648462:web:81ae0f461787843b661c5f",
-    measurementId: "G-X3SWND29VF"
-  };
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
